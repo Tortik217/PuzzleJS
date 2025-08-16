16c936088caad960d8d5b095feb4591a8222f26d
tortik217 <ivan.koledov2012@yandex.ru>
2025-08-16 15:57:27 +0300

    add grid and position

--- src/pages/components/MainPuzzlePanel/MainPazzlePanel.jsx
+++ src/pages/components/MainPuzzlePanel/MainPazzlePanel.jsx
@@ -1,25 +1,38 @@
-// import { useOutletContext } from "react-router-dom";
+import {useOutletContext} from "react-router-dom";
 import "./MainPuzzlePanel.css";
 
-export default function MainPuzzlePanel({count}) {
-  // const { imageSrc } = useOutletContext();
+export default function MainPuzzlePanel() {
 
-
+  const {count, imageSrc} = useOutletContext();
+  let gridSize = Math.sqrt(Number(count));
 
   return (
-    <div>
-      <h2 style={{ textAlign: "center" }}>Main Puzzle Panel</h2>
-      {/* {imageSrc && <img src={imageSrc} alt="From Root" width="800" />} */}
-      <div className="divs d-flex flex-column gap-2">
-        {/* <div className="icon icon1 border border-primary" />
-        <div className="icon icon2 border border-primary" />
-        <div className="icon icon3 border border-primary" />
-        <div className="icon icon4 border border-primary" />
-        <div className="icon icon5 border border-primary" />
-        <div className="icon icon6 border border-primary" />
-        <div className="icon icon7 border border-primary" /> */}
-        
+      <div className="puzzles_wrapper d-flex flex-column gap-3">
+        <h2 style={{textAlign: "center"}}>Main Puzzle Panel</h2>
+        <div className="puzzles_inner" style={{gridTemplateColumns: `repeat(${gridSize}, 1fr)`}}>
+          {Array.from({length: Number(count)}).map((_, i) => {
+
+            let row = Math.floor(i / gridSize);
+            let col = i % gridSize;
+            const bgPosX = (col / (gridSize - 1)) * 100;
+            const bgPosY = (row / (gridSize - 1)) * 100;
+
+            return (
+                <div
+                    key={i}
+                    className="puzzle_cell"
+                    style={{
+                      backgroundImage: `url(${imageSrc})`,
+                      backgroundPosition: `${bgPosX}% ${bgPosY}%`,
+                      backgroundSize: `${gridSize * 100}% ${gridSize * 100}%`,
+                    }}
+                >
+
+                </div>
+            )
+
+          })}
+        </div>
       </div>
-    </div>
   );
 }
--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,11 +1,11 @@
-import { useState, useEffect, useRef } from "react";
-import { Outlet } from "react-router-dom";
+import {useState, useEffect, useRef} from "react";
+import {Outlet} from "react-router-dom";
 import Header from "@/pages/components/Header/Header";
 
-export default function Root() {
+function Root() {
   const [imageSrc, setImageSrc] = useState(null);
-  const fileInputRef = useRef();
-    const [count, setCount] = useState(0);
+  let fileInputRef = useRef();
+  const [count, setCount] = useState(0);
 
   useEffect(() => {
     const storedImage = localStorage.getItem("myImage");
@@ -25,18 +25,25 @@ export default function Root() {
   }
 
   return (
-    <div className="main d-flex flex-column justify-content-center align-items-center">
-      <div className="header">
-        <Header />
+      <div className="main d-flex flex-column justify-content-center align-items-center">
+        <div className="header">
+          <Header/>
+        </div>
+        <div className="d-flex flex-column align-start gap-2 p-4">
+          <input type="file" ref={fileInputRef} className="btn btn-primary"/>
+          <button onClick={save} className="btn btn-primary">Save</button>
+          <h2 className="countOfPuzzles">Number of figures</h2>
+          <div className="form d-flex justify-content-center gap-2">
+            <button onClick={() => setCount(9)} className="btn btn-primary">9</button>
+            <button onClick={() => setCount(16)} className="btn btn-primary">16</button>
+            <button onClick={() => setCount(25)} className="btn btn-primary">25</button>
+            <button onClick={() => setCount(36)} className="btn btn-primary">36</button>
+            <button onClick={() => setCount(49)} className="btn btn-primary">49</button>
+          </div>
+        </div>
+        <Outlet context={{imageSrc, count}}/>
       </div>
-    <div className="d-flex flex-column align-start gap-2 p-4">
-      <input type="file" ref={fileInputRef} className="btn btn-primary" />
-      <button onClick={save} className="btn btn-primary">Save</button>
-      <button onClick={() => setCount(3)} className="btn btn-primary">3</button>
-      <button onClick={() => setCount(5)} className="btn btn-primary">5</button>
-      <button onClick={() => setCount(7)} className="btn btn-primary">7</button>
-    </div>
-      <Outlet context={{ imageSrc, count }} />
-    </div>
   );
 }
+
+export default Root;
